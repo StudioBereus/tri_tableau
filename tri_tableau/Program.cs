74ea605173691dd420858c6b2b46f15a3be8f252
osalysdecatarina <72741076+osalysdecatarina@users.noreply.github.com>
2020-11-28 10:51:42 +0100

    Ajoutez des fichiers projet.

---
+++ tri_tableau/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace tri_tableau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programme de tri d'un tableau");
+            int[] tableau = { 9, 10, 45, 0, 6 };
+            for (int i=0;i<tableau.Length; i++)
+            {
+                int nbmin = 0;
+                int index = 0;
+                for (int y = i; y < tableau.Length; y++) 
+                {
+                    if (y == i || tableau[y] < nbmin) 
+                    {
+                        nbmin = tableau[y];
+                        index = y;
+                    }
+                }
+                tableau[index] = tableau[i];
+                tableau[i] = nbmin;
+            }
+            foreach (int number in tableau )
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
